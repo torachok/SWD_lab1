5a3d9bfe894db5c9be5ac90ff2f63721953ea42e
admin <efiiimova@mail.ru>
2024-03-10 13:58:34 +0700

    Initial Commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,76 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{}
+Win::Win(QWidget *parent):QWidget (parent){
+    setWindowTitle("Возведение в квадрат");
+    frame = new QFrame(this);
+    frame->setFrameShadow(QFrame::Raised);
+    frame->setFrameShape(QFrame::Panel);
+    inputLabel = new QLabel("Введите число: ", this);
+    inputEdit = new QLineEdit("", this);
+    StrValidator *v = new StrValidator(inputEdit);
+    inputEdit->setValidator(v);
+    outputLabel = new QLabel("Результат: ", this);
+    outputEdit = new QLineEdit("", this);
+    nextButton = new QPushButton("Следующее", this);
+    exitButton = new QPushButton("Выход", this);
 
-MainWindow::~MainWindow() {}
+    QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
+    vLayout1->addWidget(inputLabel);
+    vLayout1->addWidget(inputEdit);
+    vLayout1->addWidget(outputLabel);
+    vLayout1->addWidget(outputEdit);
+    vLayout1->addStretch();
+
+    QVBoxLayout *vLayout2 = new QVBoxLayout();
+    vLayout2->addWidget(nextButton);
+    vLayout2->addWidget(exitButton);
+    vLayout2->addStretch();
+
+    QHBoxLayout *hLayout = new QHBoxLayout(this);
+    hLayout->addWidget(frame);
+    hLayout->addLayout(vLayout2);
+
+    begin();
+
+    connect(exitButton, SIGNAL(clicked(bool)), this, SLOT(close()));
+    connect(nextButton, SIGNAL(clicked(bool)), this, SLOT(begin()));
+    connect(inputEdit, SIGNAL(returnPressed()), this, SLOT(calc()));
+}
+
+void Win::begin(){
+    inputEdit->clear();
+    nextButton->setEnabled(false);
+    nextButton->setDefault(false);
+    inputEdit->setEnabled(true);
+    outputLabel->setVisible(false);
+    outputEdit->setVisible(false);
+    outputEdit->setEnabled(false);
+    inputEdit->setFocus();
+}
+
+void Win::calc(){
+    bool Ok = true;
+    float r,a;
+    QString str = inputEdit->text();
+    a = str.toDouble(&Ok);
+    if(Ok){
+        r = a*a;
+        str.setNum(r);
+        outputEdit->setText(str);
+        inputEdit->setEnabled(false);
+        outputLabel->setVisible(true);
+        outputEdit->setVisible(true);
+        nextButton->setDefault(true);
+        nextButton->setEnabled(true);
+        nextButton->setFocus();
+    }
+    else
+        if(!str.isEmpty())
+        {
+            QMessageBox msgBox(QMessageBox::Information,
+                               "Возведение в квадрат.",
+                               "Введено неверное значение.",
+                               QMessageBox::Ok);
+            msgBox.exec();
+        }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,14 +1,37 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 #include <QMainWindow>
+#include <QValidator>
+#include <QMessageBox>
+#include <QVBoxLayout>
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
+class Win:public QWidget{ //класс окна
+    Q_OBJECT //макрос Qt, обеспечивающий коррекное создание сигналов и слотов
+protected:
+    QFrame *frame;  //рамка
+    QLabel *inputLabel;  //метка ввода
+    QLineEdit *inputEdit;   //строчный редактор ввода
+    QLabel *outputLabel; //метка вывода
+    QLineEdit *outputEdit; //строчный редакор вывода
+    QPushButton *nextButton;    //кнопка Следующее
+    QPushButton *exitButton;    //кнопка Выход
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    Win(QWidget *parent = 0); // конструктор
+public slots:
+    void begin();//метод начальной настройки интерфейса
+    void calc();//метод реализации вычислений
 };
-#endif // MAINWINDOW_H
+
+class StrValidator:public QValidator{//класс компонента проверки кода
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable;//метод всегда принимает вводимую строку
+    }
+};
+#endif
