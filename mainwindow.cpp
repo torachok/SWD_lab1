a930a9c2d1d82c5e76b5db1be17db5045f5c9e4b
admin <efiiimova@mail.ru>
2024-03-10 14:04:59 +0700

    Commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,35 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{}
+Win::Win(QWidget *parent):QWidget(parent){
+    this->setWindowTitle("Счетчик");
+    label1 = new QLabel("Счет по 1", this);
+    label2 = new QLabel("Счет по 5", this);
+    edit1 = new Counter("0", this);
+    edit1->setReadOnly(true);
+    edit2 = new Counter("0", this);
+    edit2->setReadOnly(true);
+    calcbutton = new QPushButton("+1", this);
+    exitbutton = new QPushButton("Выход", this);
 
-MainWindow::~MainWindow() {}
+    QHBoxLayout *layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
+
+    QHBoxLayout *layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
+
+    QHBoxLayout *layout3 = new QHBoxLayout();
+    layout3->addWidget(calcbutton);
+    layout3->addWidget(exitbutton);
+
+    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
+
+    //связь для нажатия кнопки и слота закрытия окна
+    connect(calcbutton, SIGNAL(clicked(bool)), edit1, SLOT(add_one()));
+    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one()));
+    connect(exitbutton, SIGNAL(clicked(bool)), this, SLOT(close()));
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,39 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QString>
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QHBoxLayout>
 
-class MainWindow : public QMainWindow
-{
+class Counter:public QLineEdit{
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    Counter(const QString & contents, QWidget *parent = 0):QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one(){
+        QString str = text();
+        int r = str.toInt();
+        if(r!=0 && r%5 == 0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
 };
-#endif // MAINWINDOW_H
+
+class Win: public QWidget{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QLabel *label1, *label2;
+    Counter *edit1, *edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    Win(QWidget *parent = 0);
+};
+#endif
