f8e05ecd324ed0871e4510386dddaa6c3c7ac46f
admin <efiiimova@mail.ru>
2024-03-10 22:16:28 +0700

    commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,35 +1,35 @@
 #include "mainwindow.h"
 
 Win::Win(QWidget *parent):QWidget(parent){
-    this->setWindowTitle("Счетчик");
-    label1 = new QLabel("Счет по 1", this);
-    label2 = new QLabel("Счет по 5", this);
-    edit1 = new Counter("0", this);
-    edit1->setReadOnly(true);
-    edit2 = new Counter("0", this);
-    edit2->setReadOnly(true);
-    calcbutton = new QPushButton("+1", this);
-    exitbutton = new QPushButton("Выход", this);
+    this->setWindowTitle("Счетчик"); //название окна
+    label1 = new QLabel("Счет по 1", this); //отображение текста
+    label2 = new QLabel("Счет по 5", this); //отображение текста
+    edit1 = new Counter("0", this); //редактор однострочного текста для отображения сколько 1-иц
+    edit1->setReadOnly(true); //недоступность к изменению редактора однострочного текста (только чтение)
+    edit2 = new Counter("0", this); //редактор однострочного текстадля отображения, сколько 5-ок
+    edit2->setReadOnly(true); //недоступность к изменению редактора однострочного текста (только чтение)
+    calcbutton = new QPushButton("+1", this); //кнопка +1
+    exitbutton = new QPushButton("Выход", this); //кнопка выхода
 
-    QHBoxLayout *layout1 = new QHBoxLayout();
-    layout1->addWidget(label1);
-    layout1->addWidget(label2);
+    QHBoxLayout *layout1 = new QHBoxLayout(); //класс макета для выстраивания виджетов горизонтально
+    layout1->addWidget(label1); //добавление виджета отображения текста
+    layout1->addWidget(label2); //добавление виджета отображения текста
 
-    QHBoxLayout *layout2 = new QHBoxLayout();
-    layout2->addWidget(edit1);
-    layout2->addWidget(edit2);
+    QHBoxLayout *layout2 = new QHBoxLayout(); //класс макета для выстраивания виджетов горизонтально
+    layout2->addWidget(edit1); //добавление виджета для вывода результата счетчика
+    layout2->addWidget(edit2); //добавление виджета для вывода результата счетчика
 
-    QHBoxLayout *layout3 = new QHBoxLayout();
-    layout3->addWidget(calcbutton);
-    layout3->addWidget(exitbutton);
+    QHBoxLayout *layout3 = new QHBoxLayout(); //класс макета для выстраивания виджетов горизонтально
+    layout3->addWidget(calcbutton); //добавление виджета кнопки добавления +1 к счетчику
+    layout3->addWidget(exitbutton); //добавление виджета кнопки выхода
 
-    QVBoxLayout *layout4 = new QVBoxLayout(this);
-    layout4->addLayout(layout1);
-    layout4->addLayout(layout2);
-    layout4->addLayout(layout3);
+    QVBoxLayout *layout4 = new QVBoxLayout(this); //класс макета для выстраивания виджетов вертикально
+    layout4->addLayout(layout1); //добавление ячейки, содержащей другой QLayout в ряд и установки его растяжения
+    layout4->addLayout(layout2); //добавление ячейки, содержащей другой QLayout в ряд и установки его растяжения
+    layout4->addLayout(layout3); //добавление ячейки, содержащей другой QLayout в ряд и установки его растяжения
 
-    //связь для нажатия кнопки и слота закрытия окна
-    connect(calcbutton, SIGNAL(clicked(bool)), edit1, SLOT(add_one()));
-    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one()));
-    connect(exitbutton, SIGNAL(clicked(bool)), this, SLOT(close()));
+
+    connect(calcbutton, SIGNAL(clicked(bool)), edit1, SLOT(add_one())); //связи нажатия кнопки и добавление +1 к счетчику
+    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one())); //увеличение второго счётчика, если в первом случился tick_signal
+    connect(exitbutton, SIGNAL(clicked(bool)), this, SLOT(close())); //связь для нажатия кнопки и слота закрытия окна
 }
--- mainwindow.h
+++ mainwindow.h
@@ -10,31 +10,30 @@
 #include <QHBoxLayout>
 
 class Counter:public QLineEdit{
-    Q_OBJECT
+    Q_OBJECT //макрос Qt, обеспечивающий коррекное создание сигналов и слотов
 public:
-    Counter(const QString & contents, QWidget *parent = 0):QLineEdit(contents,parent){}
+    Counter(const QString & contents, QWidget *parent = 0):QLineEdit(contents,parent){} //счетчик и его конструктор
 signals:
-    void tick_signal();
+    void tick_signal(); //событие, которое говорит, что счётчик дошёл до 5
 public slots:
-    void add_one(){
-        QString str = text();
-        int r = str.toInt();
-        if(r!=0 && r%5 == 0) emit tick_signal();
-        r++;
-        str.setNum(r);
-        setText(str);
+    void add_one(){ //добавление +1 к значению
+        QString str = text(); //инициализация строки
+        int r = str.toInt(); //преобразование строки в число
+        if(r!=0 && (r+1)%5 == 0) emit tick_signal(); //если число != 0 и делится на 5 без остатка, запускаем имитацию сигнала
+        r++; //прибвление к счетчику 1
+        str.setNum(r); //преобразование строки в число
+        setText(str); //установка в поле увелиения числа
     }
 };
 
 class Win: public QWidget{
-    Q_OBJECT
+    Q_OBJECT //макрос Qt, обеспечивающий корректное создание сигналов и слотов
 protected:
-    QTextCodec *codec;
-    QLabel *label1, *label2;
-    Counter *edit1, *edit2;
-    QPushButton *calcbutton;
-    QPushButton *exitbutton;
+    QLabel *label1, *label2; //инициализация отображения текста
+    Counter *edit1, *edit2; //инициализация редактируемых строк
+    QPushButton *calcbutton; //инициализация кнопки добавления значения
+    QPushButton *exitbutton; //инициализация кнопки выхода
 public:
-    Win(QWidget *parent = 0);
+    Win(QWidget *parent = 0); //конструктор окна
 };
 #endif
